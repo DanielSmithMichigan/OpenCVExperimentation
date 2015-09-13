b8bada05b26a68d2a6edc84b2d5e0d8158dfa6e5
dansmith <dsmith@ltu.edu>
2015-09-13 09:01:01 -0400

    Fixed bug

--- whatever.cpp
+++ whatever.cpp
@@ -5,7 +5,7 @@ int main(int argc, char** argv )
 {
     IplImage* img = cvLoadImage(argv[1]);
 
-    cvNamedWindow("Esxample1", CV_WINDOW_AUTOSIZE);
+    cvNamedWindow("Example1", CV_WINDOW_AUTOSIZE);
     cvShowImage("Example1",img);
     cvWaitKey(0);
     cvReleaseImage(&img);
