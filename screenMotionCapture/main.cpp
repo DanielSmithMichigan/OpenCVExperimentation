e5fa5fec0b077046eec43f03135b7a7db8971d9a
dansmith <dsmith@ltu.edu>
2015-11-21 20:02:33 -0500

    Capturing and detecting image from screen

--- screenMotionCapture/main.cpp
+++ screenMotionCapture/main.cpp
@@ -15,76 +15,6 @@ using namespace std;
 using namespace cv;
 using namespace cv::xfeatures2d;
 
-Mat AddMatchesToImage(Mat img_object, Mat img_scene)
-{
-    if (!img_object.data || !img_scene.data) {
-        cout << "Error reading images";
-    }
-
-    int minHessian = 40;
-    Ptr<SURF> surf = SURF::create(minHessian);
-    vector<KeyPoint> keypoints_object, keypoints_scene;
-    Mat descriptors_object, descriptors_scene;
-    surf->detect(img_object, keypoints_object);
-    surf->compute(img_object, keypoints_object, descriptors_object);
-    surf->detect(img_scene, keypoints_scene);
-    surf->compute(img_scene, keypoints_scene, descriptors_scene);
-    FlannBasedMatcher matcher;
-    vector<DMatch> matches;
-    matcher.match(descriptors_object, descriptors_scene, matches);
-    double max_dist=0; double min_dist = 100;
-    for (int i = 0; i < descriptors_object.rows; i++) {
-        double dist = matches[i].distance;
-        if (dist < min_dist) min_dist = dist;
-        if (dist > max_dist) max_dist = dist;
-    }
-
-    cout << "Matches: " << matches.size() << endl;
-
-    vector<DMatch> good_matches;
-    for(int i = 0; i < descriptors_object.rows; i++) {
-        if (true || matches[i].distance < 3000 * min_dist) {
-            good_matches.push_back(matches[i]);
-        }
-    }
-
-    Mat img_matches;
-    drawMatches( img_object, keypoints_object, img_scene, keypoints_scene,
-        good_matches, img_matches, Scalar::all(-1), Scalar::all(-1),
-        vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
-
-    vector<Point2f> obj,scene;
-    for(int i = 0; i < good_matches.size(); i++) {
-        obj.push_back(keypoints_object[good_matches[i].queryIdx].pt);
-        scene.push_back(keypoints_scene[good_matches[i].trainIdx].pt);
-    }
-
-    cout << "obj: " << obj.size() << endl;
-    cout << "scene: " << scene.size() << endl;
-    cout << "Rows: " << img_object.rows << endl;
-    cout << "Cols: " << img_object.cols << endl;
-
-    Mat H = findHomography( obj, scene, CV_RANSAC );
-    cout << H << endl;
-    cout << "Size: " << H.size() << endl;
-    // -- Get the corners from the image_1 ( the object to be "detected" )
-    std::vector<Point2f> obj_corners(4);
-    obj_corners[0] = cvPoint(0,0); 
-    obj_corners[1] = cvPoint( img_object.cols, 0 );
-    obj_corners[2] = cvPoint( img_object.cols, img_object.rows ); 
-    obj_corners[3] = cvPoint( 0, img_object.rows );
-    std::vector<Point2f> scene_corners(4);
-    // perspectiveTransform( obj_corners, scene_corners, H);
-
-    //-- Draw lines between the corners (the mapped object in the scene - image_2 )
-    // line( img_matches, scene_corners[0] + Point2f( img_object.cols, 0), scene_corners[1] + Point2f( img_object.cols, 0), Scalar(0, 255, 0), 4 );
-    // line( img_matches, scene_corners[1] + Point2f( img_object.cols, 0), scene_corners[2] + Point2f( img_object.cols, 0), Scalar( 0, 255, 0), 4 );
-    // line( img_matches, scene_corners[2] + Point2f( img_object.cols, 0), scene_corners[3] + Point2f( img_object.cols, 0), Scalar( 0, 255, 0), 4 );
-    // line( img_matches, scene_corners[3] + Point2f( img_object.cols, 0), scene_corners[0] + Point2f( img_object.cols, 0), Scalar( 0, 255, 0), 4 );
-
-    return img_matches;
-}
-
 Mat ImageFromDisplay(int Width, int Height)
 {
     Display* display = XOpenDisplay(NULL);
@@ -102,13 +32,27 @@ int main(int argc, char** argv )
 {
     int width = 1500;
     int height = 500;
+    Mat img_object = imread("../images/rocks.png", CV_LOAD_IMAGE_COLOR );
+    Mat img_scene = ImageFromDisplay(width, height);  
+    cvtColor(img_scene, img_scene, CV_BGRA2BGR);
+    // img_scene.convertTo(img_scene, CV_32F);
+    // img_object.convertTo(img_object, CV_32F);
+    if (!img_object.data || !img_scene.data) {
+        cout << "Error reading images";
+        return -1;
+    }
+    Mat result;
+    matchTemplate( img_scene, img_object, result, CV_TM_SQDIFF_NORMED );
+    normalize( result, result, 0, 1, NORM_MINMAX, -1, Mat() );
+    double minVal; double maxVal; Point minLoc; Point maxLoc;
+    Point matchLoc;
 
-    // Mat object = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE );
-    Mat scene = ImageFromDisplay(width, height);
-    // Mat imageMatches = AddMatchesToImage(object, scene);
-    namedWindow("WindowTitle", WINDOW_AUTOSIZE);
-    imshow("WindowTitle",  scene);
+    minMaxLoc( result, &minVal, &maxVal, &minLoc, &maxLoc, Mat() );
+    matchLoc = minLoc;
+    rectangle( img_scene, matchLoc, Point( matchLoc.x + img_object.cols , matchLoc.y + img_object.rows ), CV_RGB(255, 255, 255), 3 );
 
+    imshow( "Good Matches & Object detection", img_scene );
+    
     waitKey(0);
     return 0;
 }
