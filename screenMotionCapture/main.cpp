b78843cba71e9dc8d009cc17af83bbfb0d98120d
dansmith <dsmith@ltu.edu>
2015-11-22 13:00:10 -0500

    Fixed bug causing program to crash

--- screenMotionCapture/main.cpp
+++ screenMotionCapture/main.cpp
@@ -17,43 +17,53 @@ Mat ImageFromDisplay(int Width, int Height)
     XWindowAttributes attributes = {0};
     XGetWindowAttributes(display, root, &attributes);
     XImage* img = XGetImage(display, root, 0, 0 , Width, Height, AllPlanes, ZPixmap);
-    XFree(img);
-    XCloseDisplay(display);
-    return Mat(Height, Width, img->bits_per_pixel > 24 ? CV_8UC4 : CV_8UC3, img->data);
-
-}
-
-Mat MatchAgainstScreen(int width, int height, Mat img_object)
-{
-    Mat img_scene = ImageFromDisplay(width, height); 
-    if (!img_scene.data) {
+    if (!img->data) {
+        cout << "No image data";
+        throw("No image data");
+    }
+    Mat scene = Mat(Height, Width, img->bits_per_pixel > 24 ? CV_8UC4 : CV_8UC3, img->data);
+    if (!scene.data) {
         cout << "Error reading scene";
         throw("Error reading scene");
-    } 
-    cvtColor(img_scene, img_scene, CV_BGRA2BGR);Mat result;
+    }
+    cvtColor(scene, scene, CV_BGRA2BGR);
+    XFree(img);
+    XCloseDisplay(display);
+    return scene;
+}
+
+Mat MatchAgainst(Mat img_scene, Mat img_object, const Scalar& color, String Name = "")
+{
+    Mat result;
     matchTemplate( img_scene, img_object, result, CV_TM_SQDIFF_NORMED );
-    normalize( result, result, 0, 1, NORM_MINMAX, -1, Mat() );
     double minVal; double maxVal; Point minLoc; Point maxLoc;
     Point matchLoc;
 
     minMaxLoc( result, &minVal, &maxVal, &minLoc, &maxLoc, Mat() );
-    matchLoc = minLoc;
-    rectangle( img_scene, matchLoc, Point( matchLoc.x + img_object.cols , matchLoc.y + img_object.rows ), CV_RGB(255, 255, 255), 3 );
+    cout << Name << ": " << minVal << endl;
+    if (minVal < .1) {
+        matchLoc = minLoc;
+        rectangle( img_scene, matchLoc, Point( matchLoc.x + img_object.cols , matchLoc.y + img_object.rows ), color, 3 );
+    }
     return img_scene;
 }
 
 int main(int argc, char** argv )
 {
-    Mat img_object = imread("../images/rocks.png", CV_LOAD_IMAGE_COLOR );
-    if (!img_object.data) {
+    Mat tin = imread("../images/tin.png", CV_LOAD_IMAGE_COLOR);
+    if (!tin.data) {
+        throw("Error reading scene");
+    }
+    Mat tin_mined = imread("../images/tin_mined.png", CV_LOAD_IMAGE_COLOR);
+    if (!tin_mined.data) {
         throw("Error reading scene");
     }
     Mat img_scene;
-    img_scene = MatchAgainstScreen(800, 800, img_object);
-    imshow( "Good Matches & Object detection", img_scene );
     while(true) {
-        waitKey(500);
-        img_scene = MatchAgainstScreen(800, 800, img_object);
+        waitKey(100);
+        Mat img_scene = ImageFromDisplay(800, 800);
+        img_scene = MatchAgainst(img_scene, tin, CV_RGB(255, 255, 255), "TIN");
+        img_scene = MatchAgainst(img_scene, tin_mined, CV_RGB(255, 0, 0), "MINED");
         imshow( "Good Matches & Object detection", img_scene );
     }
     
